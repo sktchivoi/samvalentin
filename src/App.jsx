058cb127ae80556bae28c4dc6a3fbbf289e0224b
ferrodr <fernando.rguezm@gmail.com>
2024-02-13 12:32:04 +0100

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Index from './index.jsx'
 import Acept from './acept.jsx'
 
 function App() {
   return (
     <>
-      <Router>
-        <Route exact path="/" component={Index}/>
-        <Route path="/index" component={Index}/>
-        <Route path="/acept" component={Acept}/>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Index/>}/>
+        <Route path="/index" element={<Index/>}/>
+        <Route path="/acept" element={<Acept/>}/>
+      </Routes>
     </>
   )
 }
