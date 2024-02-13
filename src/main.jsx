58cdaae524cbb2a63478e02f13c47352b9909ac4
ferrodr <fernando.rguezm@gmail.com>
2024-02-13 12:54:44 +0100

    Modificado browserrouter por hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <HashRouter><App /></HashRouter>
   </React.StrictMode>,
 )
